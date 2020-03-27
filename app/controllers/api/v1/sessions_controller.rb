7d458b9fc8a277b5cb48bc70e8d3e65559ed1af8
Nelson Miranda <nmirandaghn@gmail.com>
2020-03-27 01:07:45 -0600

    Add authorization to app

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::SessionsController < Devise::SessionsController
+  before_action :sign_in_params, only: :create
+  before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
+
+  def create
+    if @user.valid_password?(sign_in_params[:password])
+      sign_in "user", @user
+      json_response "Successfully signed in", true, {user: @user}, :ok
+    else
+      json_response "Unauthorized", false, {}, :unauthorized
+    end
+  end
+
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Logout successfully", true, {}, :ok
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:sign_in).permit(:email, :password)
+  end
+
+  def load_user
+    @user = User.find_for_database_authentication(email: sign_in_params[:email])
+
+    if @user
+      return @user 
+    else
+      json_response "Cannot get user", false, {}, :bad_request
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+
+    if @user
+      @user
+    else 
+      json_response "Invalid token", false, {}, :bad_request
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+        delete "log_out", to: "sessions#destroy"
+      end
+    end
+  end
 end
